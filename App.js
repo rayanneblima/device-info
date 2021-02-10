1b353490d66cec55b322969b4e049f66a3a943b4
Rayanne <rayanne.lima@oihandover.com>
2021-02-10 16:25:51 -0300

    Adding function to get serial number

--- App.js
+++ App.js
@@ -4,14 +4,14 @@ import {
   Text,
   View,
   TouchableOpacity,
+  PermissionsAndroid
 } from 'react-native';
 
 // device-info lib
-import DeviceInfo from 'react-native-device-info';
-import { getDevice, getPowerState, isBatteryCharging } from 'react-native-device-info';
+import { getPowerState, isBatteryCharging, getSerialNumber } from 'react-native-device-info';
 
 // auto-start lib
-import RNAutostart from 'react-native-autostart';
+import AutoStart from 'react-native-autostart';
 
 // heart-beat tutorial
 import { connect } from 'react-redux';
@@ -37,25 +37,41 @@ const styles = StyleSheet.create({
 });
 
 const App = () => {
+  const getPermissions = async () => {
+    
+      // Calling the permission function
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.READ_PHONE_STATE,
+        {
+          title: 'Example App PHONE STATE Permission',
+          message: 'Example App needs access to your PHONE STATE',
+          buttonNeutral: "Ask Me Later",
+          buttonNegative: "Cancel",
+          buttonPositive: "OK"
+        },
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        // Permission Granted
+        alert('PHONE STATE Permission granted');
+      } else {
+        // Permission Denied
+        alert('PHONE STATE Permission Denied');
+      }
+  };
+
   // device info
   const [isActive, setIsActive] = useState(false);
-  const [device, setDevice] = useState("");
   const [isCharging, setIsCharging] = useState(false);
   const [powerState, setPowerState] = useState("");
   const [batterylvl, setBatteryLvl] = useState("");
-
-  const deviceBrand = DeviceInfo.getBrand();
-  const model = DeviceInfo.getModel();
-  const type = DeviceInfo.getDeviceType();
-  const deviceId = DeviceInfo.getDeviceId();
-
+  const [serial, setSerial] = useState("");
+  
   isBatteryCharging().then((charging) => setIsCharging(charging));
-  getDevice().then((dvc) => setDevice(dvc));
-
   getPowerState().then((state) => {
     setBatteryLvl(state.batteryLevel);
     setPowerState(state.batteryState);
   });
+  
   // device info
 
   useEffect(() => {
@@ -64,6 +80,7 @@ const App = () => {
     }
     Unstoppable.startService(); // impedir que ative mais de uma vez
     setIsActive(true);
+    getSerialNumber().then((serialNumber) => setSerial(serialNumber));
   }, []);
   
   return (
@@ -77,12 +94,7 @@ const App = () => {
               <Text>Estado de carregamento: {powerState}</Text>
               <Text>Bateria: {(batterylvl*100).toFixed(0)}%</Text>
               <Text>Carregando? {isCharging ? "Sim" : "Não"}</Text>
-              <Text>----------------</Text>
-              <Text>Marca do Dispositivo: {deviceBrand}</Text>
-              <Text>Modelo: {model}</Text>
-              <Text>Dispositivo: {device}</Text>
-              <Text>Tipo: {type}</Text>
-              <Text>ID: {deviceId}</Text>
+              <Text>Serial Number: {serial}</Text>
               <Text>----------------</Text>
             </>
             :
@@ -93,6 +105,9 @@ const App = () => {
         </TouchableOpacity> */}
         <TouchableOpacity style={styles.button} onPress={() => {Unstoppable.stopService(); setIsActive(false); console.log("O serviço foi parado.")}}>
           <Text style={styles.instructions}>Stop</Text>
+        </TouchableOpacity>  
+        <TouchableOpacity style={styles.button} onPress={getPermissions}>
+          <Text>Permissions to getSerial</Text>
         </TouchableOpacity>
       </View>
     </View>
@@ -111,5 +126,4 @@ export default connect(mapStateToProps)(App);
 apenas 1 única vez;
 - só pode puxar as informações quando o dispositivo estiver plugado no usb, ou seja,
 carregando;
-- implementar o getSerialNumber() da lib device-info
 */
